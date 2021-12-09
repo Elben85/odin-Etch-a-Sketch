ecf1c1cac009bc6284d17d106d172260f619dd0d
elben85 <elbert.benedict@gmail.com>
2021-12-09 22:35:49 +0800

    Slight Adjustment

--- script.js
+++ script.js
@@ -2,15 +2,8 @@ const BLACK_WHITE = 0;
 const RAINBOW = 1;
 const GREYSCALE = 2;
 
-const GREY1 = "rgb(229, 229, 229)";
-const GREY2 = "rgb(204, 204, 204)";
-const GREY3 = "rgb(178, 178, 178)";
-const GREY4 = "rgb(153, 153, 153)";
-const GREY5 = "rgb(127, 127, 127)";
-const GREY6 = "rgb(101, 101, 101)";
-const GREY7 = "rgb(76, 76, 76)";
-const GREY8 = "rgb(50, 50, 50)";
-const GREY9 = "rgb(25, 25, 25)";
+const GREYSCALE_COLORS = ["rgb(229, 229, 229)", "rgb(204, 204, 204)", "rgb(178, 178, 178)", "rgb(153, 153, 153)",
+    "rgb(127, 127, 127)", "rgb(101, 101, 101)", "rgb(76, 76, 76)", "rgb(50, 50, 50)", "rgb(25, 25, 25)", "black"];
 
 let colorScheme = BLACK_WHITE;
 let activeColorScheme = document.getElementById("active");
@@ -73,45 +66,29 @@ function paintBlack(element) {
 }
 
 function paintGreyscale(element) {
-    if (element.style.backgroundColor === "black") {
+    const pixelColor = element.style.backgroundColor;
+    const indexNumber = GREYSCALE_COLORS.findIndex(x => x === pixelColor);
+
+    if (indexNumber === 9) {
         return;
-    } else if (element.style.backgroundColor === GREY9) {
-        element.style.backgroundColor = "black";
-    } else if (element.style.backgroundColor === GREY8) {
-        element.style.backgroundColor = GREY9;
-    } else if (element.style.backgroundColor === GREY7) {
-        element.style.backgroundColor = GREY8;
-    } else if (element.style.backgroundColor === GREY6) {
-        element.style.backgroundColor = GREY7;
-    } else if (element.style.backgroundColor === GREY5) {
-        element.style.backgroundColor = GREY6;
-    } else if (element.style.backgroundColor === GREY4) {
-        element.style.backgroundColor = GREY5;
-    } else if (element.style.backgroundColor === GREY3) {
-        element.style.backgroundColor = GREY4;
-    } else if (element.style.backgroundColor === GREY2) {
-        element.style.backgroundColor = GREY3;
-    } else if (element.style.backgroundColor === GREY1) {
-        element.style.backgroundColor = GREY2;
-    } else {
-        element.style.backgroundColor = GREY1;
     }
+
+    element.style.backgroundColor = GREYSCALE_COLORS[indexNumber + 1];
 }
 
 function clearCanvas() {
     const individualPixels = document.getElementsByClassName("individualPixel");
-
-    for (let i = 0; i < individualPixels.length; ++i) {
-        individualPixels[i].style.background = "none";
-    }
+    (Array.from(individualPixels)).forEach(pixel => {
+        pixel.style.background = "none";
+    });
 }
 
 function createIndividualCanvasPixel(size) {
-    for (let i = 0; i < size; ++i) {
+    for (let _ = 0; _ < size; ++_) {
         const canvasRow = document.createElement("div");
         canvasRow.className = "canvasRow";
 
-        for (let i = 0; i < size; ++i) {
+        for (let _ = 0; _ < size; ++_) {
             const singleCanvasPixel = document.createElement("div");
             singleCanvasPixel.className = "individualPixel";
             singleCanvasPixel.addEventListener("mouseover", () => paintPixel(singleCanvasPixel));
@@ -125,11 +102,7 @@ function createIndividualCanvasPixel(size) {
 function handleCanvasSizeChange(size) {
     sizePanel.textContent = `${size} x ${size}`;
     const oldPixel = document.getElementsByClassName("canvasRow");
-
-    for (let i = oldPixel.length - 1; i >= 0; --i) {
-        oldPixel[i].remove();
-    }
-
+    (Array.from(oldPixel)).forEach(pixel => pixel.remove());
     createIndividualCanvasPixel(size);
 }
 
